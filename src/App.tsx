e8d00133c4c3d38c0744f916ed746c098163a45f
ensomnatt <ensomnatt@protonmail.com>
2026-01-26 21:56:32 +0300

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { useEffect } from "react";
+
 const tg = Telegram.WebApp;
 
 function App() {
+  useEffect(() => {
+    tg.ready();
+  }, [])
+
   const onClose = () => {
     tg.close();
   }
+
   return (
     <div className="bg-[var(--tg-theme-bg-color)] text-[var(--tg-theme-text-color)]">
       <button onClick={onClose}>
