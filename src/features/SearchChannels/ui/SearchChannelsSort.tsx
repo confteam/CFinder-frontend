927e3b86a62b47fcb611a6d7cf31c37acf610f1a
ensomnatt <ensomnatt@protonmail.com>
2026-02-09 19:43:20 +0300

    add animation for sorting component

--- src/features/SearchChannels/ui/SearchChannelsSort.tsx
+++ src/features/SearchChannels/ui/SearchChannelsSort.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/shared";
 import { useSearchStore } from "../model/store";
 import { FaArrowDown, FaArrowUp } from "react-icons/fa";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface Props {
   className?: string;
@@ -9,16 +10,44 @@ interface Props {
 export function SearchChannelsSort({ className }: Props) {
   const { sort, toggleSort } = useSearchStore();
 
+  const text =
+    sort === "asc"
+      ? "Сначала меньше подписчиков"
+      : "Сначала больше подписчиков";
+
   return (
     <div className={`flex gap-5 items-center ${className}`}>
-      <h3>{sort === "asc" ? "Сначала меньше подписчиков" : "Сначала больше подписчиков"}</h3>
+      <div className="relative overflow-hidden">
+        <AnimatePresence mode="wait">
+          <motion.h3
+            key={sort}
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 10 }}
+            transition={{ duration: 0.2 }}
+          >
+            {text}
+          </motion.h3>
+        </AnimatePresence>
+      </div>
+
       <Button
         size="sm"
         variant="primary"
         className="px-5 py-2"
         onClick={toggleSort}
       >
-        {sort === "asc" ? <FaArrowUp /> : <FaArrowDown />}
+        <AnimatePresence mode="wait" initial={false}>
+          <motion.div
+            key={sort}
+            initial={{ rotate: -90, opacity: 0 }}
+            animate={{ rotate: 0, opacity: 1 }}
+            exit={{ rotate: 90, opacity: 0 }}
+            transition={{ duration: 0.2 }}
+          >
+            {sort === "asc" ? <FaArrowUp /> : <FaArrowDown />}
+          </motion.div>
+        </AnimatePresence>
       </Button>
     </div>
   );
